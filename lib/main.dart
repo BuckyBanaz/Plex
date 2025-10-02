14dd41102310380dc69cc3ce540cbd23d6e9db00
Parikshit Verma <parikshitverma865@gmail.com>
2025-10-02 14:13:06 +0530

    feat: Integrate Firebase and remove desktop/web support
    This commit introduces Firebase to the project and removes support for Linux, macOS, Windows, and web platforms.
    
    - Adds `firebase_core` and `firebase_app_distribution` as dependencies.
    - Integrates Firebase initialization in the `main.dart` entrypoint.
    - Adds Google Services plugin configuration for Android.
    - Deletes all platform-specific code and configuration files for Linux, macOS, Windows, and web, as they are no longer supported.

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,15 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:plex_user/services/translations/app_translations.dart';
 import 'package:plex_user/services/translations/locale_controller.dart';
 import 'package:sizer/sizer.dart';
-
+import 'firebase_options.dart'; // ye file Firebase CLI se generate hoti hai
+import 'package:firebase_core/firebase_core.dart';
 import 'core/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   // 1) Load translation JSONs
   await TranslationService.init();
 
